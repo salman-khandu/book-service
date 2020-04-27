9b4c5326d7e6a9e3a252d782438685955af13bb8
salman.khandu <salman.khandu@peaas.co>
2020-04-27 22:37:58 +0530

    book service

---
+++ src/main/java/com/example/book/BookApplication.java
@@ -0,0 +1,39 @@
+package com.example.book;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dto.book.Book;
+
+@EnableEurekaClient
+@RestController
+@RequestMapping("/books")
+@SpringBootApplication
+public class BookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookApplication.class, args);
+	}
+
+	private List<Book> bookList = Arrays.asList(new Book(1L, "Baeldung goes to the market", "Tim Schimandle"),
+			new Book(2L, "Baeldung goes to the park", "Slavisa"));
+
+	@GetMapping("")
+	public List<Book> findAllBooks() {
+		return bookList;
+	}
+
+	@GetMapping("/{bookId}")
+	public Book findBook(@PathVariable Long bookId) {
+		return bookList.stream().filter(b -> b.getId().equals(bookId)).findFirst().orElse(null);
+	}
+
+}
